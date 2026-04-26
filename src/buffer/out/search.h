2665d016371df736835b5c3e22ed7cd80d1786e0
Ebenezer <Oladivine0306@gmail.com>
2026-04-26 00:21:25 -0500

    [sol] Add support for regex search to conhost and Terminal

--- src/buffer/out/search.h
+++ src/buffer/out/search.h
@@ -1,66 +1,54 @@
-/*++
-Copyright (c) Microsoft Corporation
-Licensed under the MIT license.
+commit a5ab8552c3a014956ff0657cf4a1396897d24fd3
+Author: Ebenezer <Oladivine0306@gmail.com>
+Date:   Sun Apr 26 00:14:47 2026 -0500
 
-Module Name:
-- search.h
+    [sol] Add support for regex search to conhost and Terminal
 
-Abstract:
-- This module is used for searching through the screen for a substring
-
-Author(s):
-- Michael Niksa (MiNiksa) 20-Apr-2018
-
-Revision History:
-- From components of find.c by Jerry Shea (jerrysh) 1-May-1997
---*/
-
-#pragma once
-
-#include "textBuffer.hpp"
-#include "../renderer/inc/IRenderData.hpp"
-
-enum class SearchFlag : unsigned int
-{
-    None = 0,
-
-    CaseInsensitive = 1 << 0,
-    RegularExpression = 1 << 1,
-};
-
-DEFINE_ENUM_FLAG_OPERATORS(SearchFlag);
-
-class Search final
-{
-public:
-    Search() = default;
-
-    bool IsStale(const Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, SearchFlag flags) const noexcept;
-    bool Reset(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, SearchFlag flags, bool reverse);
-
-    void MoveToCurrentSelection();
-    void MoveToPoint(til::point anchor) noexcept;
-    void MovePastPoint(til::point anchor) noexcept;
-    void FindNext(bool reverse) noexcept;
-
-    const til::point_span* GetCurrent() const noexcept;
-    bool SelectCurrent() const;
-
-    const std::vector<til::point_span>& Results() const noexcept;
-    std::vector<til::point_span>&& ExtractResults() noexcept;
-    ptrdiff_t CurrentMatch() const noexcept;
-    bool IsOk() const noexcept;
-
-private:
-    // _renderData is a pointer so that Search() is constexpr default constructable.
-    Microsoft::Console::Render::IRenderData* _renderData = nullptr;
-    std::wstring _needle;
-    SearchFlag _flags{};
-    uint64_t _lastMutationId = 0;
-
-    bool _ok{ false };
-    std::vector<til::point_span> _results;
-    ptrdiff_t _index = 0;
-    ptrdiff_t _step = 0;
-};
-// HELIX_MARKER_regex_search_v1
+diff --git a/src/buffer/out/search.h b/src/buffer/out/search.h
+index c991eb92..763b5d40 100644
+--- a/src/buffer/out/search.h
++++ b/src/buffer/out/search.h
+@@ -20,13 +20,23 @@ Revision History:
+ #include "textBuffer.hpp"
+ #include "../renderer/inc/IRenderData.hpp"
+ 
++enum class SearchFlag : unsigned int
++{
++    None = 0,
++
++    CaseInsensitive = 1 << 0,
++    RegularExpression = 1 << 1,
++};
++
++DEFINE_ENUM_FLAG_OPERATORS(SearchFlag);
++
+ class Search final
+ {
+ public:
+     Search() = default;
+ 
+-    bool IsStale(const Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, bool caseInsensitive) const noexcept;
+-    bool Reset(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, bool caseInsensitive, bool reverse);
++    bool IsStale(const Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, SearchFlag flags) const noexcept;
++    bool Reset(Microsoft::Console::Render::IRenderData& renderData, const std::wstring_view& needle, SearchFlag flags, bool reverse);
+ 
+     void MoveToCurrentSelection();
+     void MoveToPoint(til::point anchor) noexcept;
+@@ -39,14 +49,16 @@ public:
+     const std::vector<til::point_span>& Results() const noexcept;
+     std::vector<til::point_span>&& ExtractResults() noexcept;
+     ptrdiff_t CurrentMatch() const noexcept;
++    bool IsOk() const noexcept;
+ 
+ private:
+     // _renderData is a pointer so that Search() is constexpr default constructable.
+     Microsoft::Console::Render::IRenderData* _renderData = nullptr;
+     std::wstring _needle;
+-    bool _caseInsensitive = false;
++    SearchFlag _flags{};
+     uint64_t _lastMutationId = 0;
+ 
++    bool _ok{ false };
+     std::vector<til::point_span> _results;
+     ptrdiff_t _index = 0;
+     ptrdiff_t _step = 0;
